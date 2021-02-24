cbbc7571f97eda476a5e5c881c6d3aa3104d8349
life4music <mawlodkowska@gmail.com>
2021-02-24 12:57:29 +0100

    stoper

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,37 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import SwitchButton from './SwitchButton';
 
 class App extends Component {
+  state = {
+    time: 0,
+    active: false
+  }
+
+  addSecond = () => {
+    this.setState((prevState) => ({
+      time: prevState.time + 1
+    }))
+  }
+
+  handleClick = () => {
+    if(this.state.active) {
+      clearInterval(this.interval)
+    } else {
+      this.interval = setInterval(() => this.addSecond(), 1000)
+    }
+
+    this.setState({
+      active: !this.state.active
+    })
+  }
+
   render() {
-    return (
-      <Fragment>
-        <div className="app">
-          Aplikacja
-        </div>
-      </Fragment>
+    return(
+      <>
+        <p>{this.state.time}</p>
+        <SwitchButton active={this.state.active} click={this.handleClick}/>
+      </>
     );
   }
 }
