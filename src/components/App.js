2742e7520a07f2dd8dae49be11daef012b744228
life4music <mawlodkowska@gmail.com>
2021-02-24 12:28:35 +0100

    myFirtApp

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component, Fragment} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="app">
+          Aplikacja
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
